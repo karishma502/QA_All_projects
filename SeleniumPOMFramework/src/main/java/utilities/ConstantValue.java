b491b28d74af4e0ea4744c9584563ffb90d446dd
karishma <karishmakate31@gmail.com>
2021-10-16 10:40:38 +0530

    done

---
+++ SeleniumPOMFramework/src/main/java/utilities/ConstantValue.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public class ConstantValue {
+	public static final String userName="kamath.s2020@rediffmail.com";
+	public static final String password="Welcome1$";
+	//public static final String userName="anshulc55";
+	//public static final String password="Test@1234";
+
+}
